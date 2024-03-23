0a43719daab3437f5a75eb39a60eab9ab057907e
Florin Pinta <xttrust@live.com>
2024-03-23 19:27:39 +0000

    Learging the filter() in javascript

---
+++ filter.js
@@ -0,0 +1,68 @@
+
+/**
+ * To run this file in Gitpod, use the 
+ * command node filter.js in the terminal
+ */
+
+
+// Simple Filtering
+const people = [
+    {
+        name: 'Michael',
+        age: 23,
+    },
+    {
+        name: 'Lianna',
+        age: 16,
+    },
+    {
+        name: 'Paul',
+        age: 18,
+    },
+];
+
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === 'Paul');
+console.log(paul);
+
+// Complex Filtering
+const students = [
+    {
+        id: 1,
+        name: 'Mark',
+        profession: 'Developer',
+        skills: [
+            { name: 'javascript', yrsExperience: 1 },
+            { name: 'html', yrsExperience: 5 },
+            { name: 'css', yrsExperience: 3 },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Ariel',
+        profession: 'Developer',
+        skills: [
+            { name: 'javascript', yrsExperience: 0 },
+            { name: 'html', yrsExperience: 4 },
+            { name: 'css', yrsExperience: 2 },
+        ]
+    },
+    {
+        id: 3,
+        name: 'Jason',
+        profession: 'Designer',
+        skills: [
+            { name: 'javascript', yrsExperience: 1 },
+            { name: 'html', yrsExperience: 1 },
+            { name: 'css', yrsExperience: 5 },
+        ]
+    },
+];
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const canditates = students.filter(hasStrongSkills);
+const names = canditates.map(c => c.name);
+console.log(canditates);
+console.log(names);
