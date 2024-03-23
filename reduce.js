2f0ab2ece35032897329a120eda7e319990e7790
Florin Pinta <xttrust@live.com>
2024-03-23 20:58:58 +0000

    Learning the reduce() method in javascript

---
+++ reduce.js
@@ -0,0 +1,39 @@
+  
+/**
+ * To run this file in Gitpod, use the 
+ * command node reduce.js in the terminal
+ */
+
+
+// Summing an array of numbers:
+const nums = [0,1,2,3,4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
+
+const teamMembers = [
+    {
+      name: 'Andrew',
+      profession: 'Developer',
+      yrsExperience: 5
+    },
+    {
+      name: 'Ariel',
+      profession: 'Developer',
+      yrsExperience: 7
+    },
+    {
+      name: 'Michael',
+      profession: 'Designer',
+      yrsExperience: 1
+    },
+    {
+      name: 'Kelly',
+      profession: 'Designer',
+      yrsExperience: 3
+    }
+  ];
+  
+  // Totaling a specific object property
+  
+  
+  // Grouping by a property, and totaling it too
